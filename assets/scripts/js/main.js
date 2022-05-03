f202f3fc3887599e2aaa67310a9d9e8e8d4331c8
Redo76 <redouane.hamadache1@gmail.com>
2022-05-03 14:23:32 +0200

    Ajout de quelques commentaires en CSS et ajout de la partie JS de la modal

--- assets/scripts/js/main.js
+++ assets/scripts/js/main.js
@@ -1,7 +1,26 @@
 // Déclaration des variables
 
-const btn_cv = document.getElementById("cv_btn");
+const colorSwitch = document.getElementById("colorSwicth")
+const btnCv = document.getElementById("cv_btn");
+const modalCv = document.getElementById("modal_cv");
+const backDrop = document.getElementById("backdrop");
+const closeModalBtn = document.getElementById("closemodal");
 
-btn_cv.addEventListener("click", ()=>{
-    
+
+btnCv.addEventListener("click", ()=>{
+    afficherModalCv();
 })
+
+closeModalBtn.addEventListener("click", ()=>{
+    retirerModalCv();
+})
+
+function afficherModalCv(){
+    backDrop.style.display = "block";
+    modalCv.style.display = "block";
+}
+
+function retirerModalCv(){
+    backDrop.style.display = "none";
+    modalCv.style.display = "none";
+}
