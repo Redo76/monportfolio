7cfb308550d0d695b126808a0cde9eb5085d0334
Redo76 <redouane.hamadache1@gmail.com>
2022-05-03 16:56:08 +0200

    Modification des variables css de couleurs pour que le site correspond à la maquette figma

--- assets/scripts/js/main.js
+++ assets/scripts/js/main.js
@@ -1,11 +1,23 @@
 // Déclaration des variables
 
-const colorSwitch = document.getElementById("colorSwicth")
+const colorSwitch = document.getElementById("colorSwitch")
 const btnCv = document.getElementById("cv_btn");
 const modalCv = document.getElementById("modal_cv");
 const backDrop = document.getElementById("backdrop");
 const closeModalBtn = document.getElementById("closemodal");
 
+colorSwitch.addEventListener("click", ()=>{
+    let themeDeBase = document.documentElement.getAttribute("data-theme");
+    let themeSelect = "light";
+
+    if (themeDeBase === "dark"){
+        themeSelect = "light";
+    } else {
+        themeSelect = "dark";
+    }
+
+    document.documentElement.setAttribute("data-theme", themeSelect)
+})
 
 btnCv.addEventListener("click", ()=>{
     afficherModalCv();
