3a18219387c0faecb6cc93cd832f342c6069697b
Redo76 <redouane.hamadache1@gmail.com>
2022-05-05 17:21:01 +0200

    Partie responsive toujours en cours et ajout d'une barre de scroll en js en haut de l'écran

--- assets/scripts/js/main.js
+++ assets/scripts/js/main.js
@@ -5,6 +5,27 @@ const btnCv = document.getElementById("cv_btn");
 const modalCv = document.getElementById("modal_cv");
 const backDrop = document.getElementById("backdrop");
 const closeModalBtn = document.getElementById("closemodal");
+const html = document.querySelector("html");
+const barreScroll = document.querySelector(".scroll");
+
+// Barre de défilement en haut de l'écran 
+
+window.addEventListener("scroll", ()=>{
+    const a = html.scrollTop;
+    
+    const b = html.scrollHeight ;
+    
+    const c = html.clientHeight;
+
+    if (a === 0){
+        barreScroll.style.width = "0%"
+    } else {
+        let pourcentage = (a + c) / b * 100;
+        barreScroll.style.width = `${pourcentage}%`;
+        console.log(parseInt(pourcentage)); 
+    }
+})
+
 
 colorSwitch.addEventListener("click", ()=>{
     let themeDeBase = document.documentElement.getAttribute("data-theme");
